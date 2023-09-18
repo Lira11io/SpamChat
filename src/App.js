e3d1297ffac63e97101bfb004cb2e818fd12ddb6
Lira11io <Lira11pm@mail.ru>
2023-09-18 23:35:00 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style/App.css";
 
 function App() {
+  const [message, setMessage] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const checkSpam = (str) => {
+    const regex = /viagra|xxx/gi;
+    return str.replace(regex, "***");
+  };
+
+  const handlInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddMessege = (e) => {
+    e.preventDefault();
+    const newMessege = checkSpam(inputValue);
+    setMessage([newMessege, ...message]);
+    setInputValue("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Форум с комментариями</h1>
       </header>
+      <main className="App-main">
+        <div className="container">
+          <div className="comments-chat">
+            <div className="comments-chat-input">
+              {message.map((message, index) => (
+                <p key={index}>{message}</p>
+              ))}
+            </div>
+          </div>
+          <div className="comments">
+            <div className="comments-form">
+              <label className="comments-form-label" htmlFor="form-input">
+                Добавьте свой комментарий:
+              </label>
+              <textarea
+                className="comments-form-input"
+                id="form-input"
+                value={inputValue}
+                onChange={handlInputChange}
+              />
+            </div>
+            <button className="comments_btn" onClick={handleAddMessege}>
+              SEND
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
